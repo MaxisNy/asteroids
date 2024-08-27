bde95c133c2b2df5b814194edc96451afca829fb
Maxim Nyanin <maximnyanin@gmail.com>
2024-08-27 16:12:46 -0400

    Update game loop and create sprite groups

--- main.py
+++ main.py
@@ -13,10 +13,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
+    Player.containers = (updatable, drawable)
     while True:
         screen.fill('black')
-        player.draw(screen)
-        player.update(dt)
+        # update all player objects 
+        for obj in Player.containers[0]:
+            obj.update(dt)
+        # draw all player obejcts
+        for obj in Player.containers[1]:
+            obj.draw(screen)
         # screen render function
         pygame.display.flip()
         for event in pygame.event.get():
--- player.py
+++ player.py
@@ -4,6 +4,8 @@ import pygame
 
 class Player(CircleShape):
 
+    containers = ()
+
     def __init__(self, x, y) -> None:
         super().__init__(x, y, PLAYER_RADIUS)
         self.position = pygame.Vector2(x, y)
@@ -25,7 +27,21 @@ class Player(CircleShape):
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
-            self.rotate(dt * -1)
-        if keys[pygame.K_d]:
-            self.rotate(dt)
+        # inverse rotation if moving backwards
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
+            if keys[pygame.K_a]:
+                self.rotate(dt)
+            if keys[pygame.K_d]:
+                self.rotate(dt * -1)
+        else:
+            if keys[pygame.K_a]:
+                self.rotate(dt * -1)
+            if keys[pygame.K_d]:
+                self.rotate(dt)
+            if keys[pygame.K_w]:
+                self.move(dt)
+        
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
