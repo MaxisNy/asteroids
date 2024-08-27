8f554a7d234bcc06ac8605ffeaf49799ba3f67b7
Maxim Nyanin <maximnyanin@gmail.com>
2024-08-27 14:51:42 -0400

    Create a clock object to set FPS

--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 import pygame
 from constants import *
 
+clock = pygame.time.Clock()
+dt = 0
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,6 +16,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
